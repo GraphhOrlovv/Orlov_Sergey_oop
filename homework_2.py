31a7350ad7362dcf0a1b6afa75dd0f6356f45ac8
Сергей Орлов <seregaorlov.gus@yandex.ru>
2026-07-08 22:42:04 +0300

    homework_2 is done

--- homework_2.py
+++ homework_2.py
@@ -71,16 +71,91 @@
 
 """4. Создай класс Person, в котором метод __init__() принимает имя и возраст и сохраняет их как атрибуты объекта.
 Добавь метод show_info(), который выводит строку "Имя: <name>, возраст: <age>". Создай объект и вызови метод.
-======================================
-5. Добавь в класс Person метод __del__(), который выводит сообщение "Удалён объект: <имя>",
+======================================"""
+
+# class Person:
+#
+#     def __init__(self, name: str, age: int | float) -> None:
+#         self.name = name
+#         self.age = age
+#
+#     def show_info(self) -> None:
+#         print(f"Имя: {self.name}, возраст: {self.age}")
+#
+# person_1 = Person('Олег', 25)
+#
+# person_1.show_info()
+
+"""5. Добавь в класс Person метод __del__(), который выводит сообщение "Удалён объект: <имя>",
 где <имя> — значение поля name. Создай и удали объект вручную с помощью del.
-======================================
-6. Создай класс Rectangle с инициализацией по умолчанию: ширина 1, высота 1.
+======================================"""
+
+# class Person:
+#
+#     def __init__(self, name: str, age: int | float) -> None:
+#         self.name = name
+#         self.age = age
+#
+#     def show_info(self) -> None:
+#         print(f"Имя: {self.name}, возраст: {self.age}")
+#
+#     def __del__(self):
+#         print(f"Удалён объект {self.name}")
+#
+# person_1 = Person('Илья', 30)
+# print()
+# person_1.show_info()
+#
+# person_2 = Person('Ваня', 18)
+# del person_2
+# # person_2.show_info() # Не получится, так как такого объекта уже нет
+
+"""6. Создай класс Rectangle с инициализацией по умолчанию: ширина 1, высота 1.
 Добавь метод area(), который возвращает площадь прямоугольника.
 Проверь работу с прямоугольником без аргументов и с заданной шириной и высотой.
-======================================
-7. Создай класс Logger, который всегда возвращает один и тот же объект.
+======================================"""
+
+# class Rectangle:
+#
+#     def __init__(self, width: int | float =1, height: int | float =1) -> None:
+#         self.width = width
+#         self.height = height
+#
+#     def area(self) -> int | float:
+#         return self.width * self.height
+#
+# rectangle_1 = Rectangle()
+#
+# print(f"Площадь прямоугольника с заданной шириной {rectangle_1.width} "
+#       f"и высотой {rectangle_1.height} равна: {rectangle_1.area()}\n")
+#
+# rectangle_2 = Rectangle(3, 5)
+#
+# print(f"Площадь прямоугольника с шириной {rectangle_2.width} "
+#       f"и высотой {rectangle_2.height} равна: {rectangle_2.area()}")
+
+"""7. Создай класс Logger, который всегда возвращает один и тот же объект.
 При создании экземпляра в __new__ выводи Создание логгера,
 а при вызове __init__ — Инициализация логгера.
 ======================================
 """
+
+# class Logger:
+#     instance = None
+#
+#     def __new__(cls, *args, **kwargs):
+#         print("Создание логгера")
+#         if cls.instance is None:
+#             cls.instance = super().__new__(cls)
+#         return cls.instance
+#
+#     def __init__(self):
+#         print("Инициализация логгера")
+#
+# object_1 = Logger()
+#
+# object_2 = Logger()
+#
+# print(id(object_1))
+#
+# print(id(object_2))
